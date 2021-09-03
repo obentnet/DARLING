b9db215718d5a097cb08e432f6bb23418b899a6c
ObentNet <37967378+obentnet@users.noreply.github.com>
2021-09-03 09:39:30 +0800

    增加阴影

--- copyright.php
+++ copyright.php
@@ -2,6 +2,6 @@
     if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 ?>
 
-<div class="copy-info">
+<div class="copy-info zero-shadow">
     <p>本文章由<?php $this->author();?>编写,遵守<a href="" class="zero-a">CC BY-NC-SA 4.0</a>,转载请注明来源</p>
 </div>
