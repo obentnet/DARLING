6abf57d36086fdc8012670346b3d0470a1e945f9
ObentNet <37967378+obentnet@users.noreply.github.com>
2021-09-03 08:28:21 +0800

    Update DARLING.js

--- DARLING.js
+++ DARLING.js
@@ -1,1 +1,6 @@
+/**
+ * DARLING Theme.
+ */
 console.log("%c theme: DARLING %c Links: https://yujienb.cn ","color:#fff;background-color:#81D4FA;padding:5px","margin: 1em 0; padding: 5px 0; background: #efefef;");
+
+/** 删除冗余内容 */
