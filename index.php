70fb805918aeaad7f2541761bc3b7b2ac8bdf5f8
ObentNet <37967378+obentnet@users.noreply.github.com>
2021-09-03 08:20:57 +0800

    增加分页
    修复小bug

--- index.php
+++ index.php
@@ -21,6 +21,10 @@ $this->need('header.php');
             <div class="state"><?php $this->date(); ?> | <span class="categroy-a-link-index"><?php $this->category(','); ?></span> | <?php $this->commentsNum('暂无评论~', '1 条评论', '%d 条评论'); ?></div>
         </article>
     <?php endwhile;?>
+    <div class="page-num-link">
+        <?php $this->pageLink('下一页','next'); ?>
+        <?php $this->pageLink('上一页'); ?>
+    </div>
     </div>
 <?php endif;?>
 <?php $this->need('footer.php')?>
