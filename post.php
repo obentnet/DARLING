abc63822911ebade51c1d73300c3139b90473637
ObentNet <37967378+obentnet@users.noreply.github.com>
2021-09-03 09:41:23 +0800

    增加阴影

--- post.php
+++ post.php
@@ -2,7 +2,7 @@
     if (!defined('__TYPECHO_ROOT_DIR__')) exit;
     $this->need('header.php');
 ?>
-<div class="article-main">
+<div class="article-main zero-shadow">
     <div class="post-info">
         <h1><?php $this->title();?></h1>
         <?php $this->date(); ?> | <span class="categroy-a-link"><?php $this->category(','); ?></span> | <?php $this->commentsNum('暂无评论~', '1 条评论', '%d 条评论'); ?>
