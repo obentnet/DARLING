dfad1602fd58249351c16e1af4cd056e5f0957a3
ObentNet <37967378+obentnet@users.noreply.github.com>
2021-09-06 08:16:12 +0800

    Update header.php

--- header.php
+++ header.php
@@ -20,6 +20,29 @@
     <link rel="shortcut icon" href="<?php $this->options->themeUrl('favicon.ico')?>" type="image/x-icon">
     <?php endif;?>
     <?php $this->header(); ?>
+    <style>
+        /** 全局字体粗细 */
+        html{font-weight: <?php $this->options->fontweight(); ?>;}
+        /** 电脑端背景 */
+        <?php if($this->options->custom_pc_background):?>
+        body{background-image: url(<?php $this->options->custom_pc_background();?>);}
+        <?php else:?>
+         body{background-image: url(<?php $this->options->themeurl('./images/bg.png')?>);}
+        <?php endif;?>
+        /** 移动端背景 */
+        <?php if($this->options->custom_mobile_background):?>
+        @media screen and (max-width: 768px){
+        body{background-image: url(<?php $this->options->custom_mobile_background();?>);}
+        }
+        <?php else:?>
+        @media screen and (max-width: 768px){
+            body{background-image: url(<?php $this->options->themeurl('./images/bg-mobile.png')?>);}
+        }
+        <?php endif;?>
+    </style>
+    <?php if($this -> options -> custom_css): ?>
+        <style><?php $this -> options -> custom_css(); ?></style>
+    <?php endif; ?>
 </head>
 <body id="body">
     <header>
