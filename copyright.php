e70f5358b26075e45b4e39fb613f0b1faacd230f
ObentNet <37967378+obentnet@users.noreply.github.com>
2021-09-06 08:14:50 +0800

    Update copyright.php
    美化

--- copyright.php
+++ copyright.php
@@ -3,5 +3,5 @@
 ?>
 
 <div class="copy-info zero-shadow">
-    <p>本文章由<?php $this->author();?>编写,遵守<a href="" class="zero-a">CC BY-NC-SA 4.0</a>,转载请注明来源</p>
+    <p>本文章由<?php $this->author();?>编写,遵守 <a href="" class="zero-a">CC BY-NC-SA 4.0</a> ,转载请注明来源</p>
 </div>
