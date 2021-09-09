245e148a24bdf163056a471a3a748f2dbcbc3512
ObentNet <37967378+obentnet@users.noreply.github.com>
2021-09-09 19:06:57 +0800

    🧨搬家

--- footer.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit;?>
-    <footer>
-        <div class="copy" id="copyright"></div>
-    </footer>
-    <script src="<?php $this->options->themeUrl('DARLING.js')?>"></script>
-    <?php $this -> options -> custom_script(); ?>
-    <?php $this -> footer(); ?>
-</body>
-</html>
