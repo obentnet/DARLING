5d5d6e4267db7e0ebdb95e79e32f971d0f48c52a
ObentNet <37967378+obentnet@users.noreply.github.com>
2021-09-09 19:07:10 +0800

    小修

--- DARLING.php
+++ DARLING.php
@@ -1,9 +1,10 @@
 <?php
-/**
- * DARLING.PHP
- * By: 狱杰1Jnver
- * Link: https://yujienb.cn/
- */
-// darling info
-    $now_version = '1.0.2'
+    /**
+    * Made with love By 狱杰1Jnver
+    * Link: https://yujienb.cn/
+    * Theme: DARLING
+    */
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+    // darling info
+    $now_version = '1.3';
 ?>
