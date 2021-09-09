396f54705f4f5cf46c2ea476067247ca1dfaa6e8
ObentNet <37967378+obentnet@users.noreply.github.com>
2021-09-09 19:08:23 +0800

    还没完善~

---
+++ links.php
@@ -0,0 +1,13 @@
+<?php
+    if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+    /**
+     * 友情链接
+     * @package custom
+     */
+    /**
+    * Made with love By 狱杰1Jnver
+    * Link: https://yujienb.cn/
+    * Theme: DARLING
+    */
+?>
+<?php Links_Plugin::output(); ?>
