4f33d650934737c4e032d6edc72d185dcbc157bc
ObentNet <37967378+obentnet@users.noreply.github.com>
2021-10-26 09:07:43 +0800

    v1.0.3
    增加字体

--- includes/auto-echo.php
+++ includes/auto-echo.php
@@ -7,6 +7,21 @@
     if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 ?>
 <style>
+    /* 华康少女体 */
+    @font-face{
+        font-family: hksnt;
+        src: url('<?php $this->options->themeUrl('/static/hksnt.ttc')?>');
+    }
+    /* 庞门正道 */
+    @font-face {
+        font-family: pmzd;
+        src: url('<?php $this->options->themeUrl('/static/pmzd.ttf')?>');
+    }
+    /* 本墨绪圆 */
+    @font-face {
+        font-family: bmxy;
+        src: url('<?php $this->options->themeUrl('/static/bmxy.ttf')?>');
+    }
     .zero-post-bg{
         background-image:url(<?php $this->options->themeUrl('/images/post-background.png');?>);
     }
